59e1caf911446cae73fa2aa2325fba88c6b9a1ae
Yamkela5 <owamjames1@gmail.com>
2019-05-20 11:28:15 +0200

    adding  files

--- greet-in-java/src/main/resources/db/migration/V1__Create_greeted_table.sql
+++
@@ -1,5 +0,0 @@
-create table GreetUser (
-                    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                    name varchar(40) NOT NULL,
-                    user_count int NOT NULL
-                );
